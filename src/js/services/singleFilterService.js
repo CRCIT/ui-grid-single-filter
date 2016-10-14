2138dcc78c805797bcb3ef112a2602077f565b10
plopez <plopez@crcit.es>
2016-10-14 15:56:21 +0200

    Refactorización del SingleFilterService

--- src/js/services/singleFilterService.js
+++ src/js/services/singleFilterService.js
@@ -12,111 +12,52 @@
       //service body
 
       return {
-        singleFilter:singleFilter,
-        getRenderedCellByColumnDef:getRenderedCellByColumnDef
+        singleFilter:singleFilter
       };
-
-
-      function getRenderedCellByColumnDef(entity,columnDef, row) {
-
-        if(!columnDef) return '';
-
-        if(columnDef.cellTemplate) {
-
-          var html = columnDef.cellTemplate.replace(uiGridConstants.MODEL_COL_FIELD, Grid.prototype.getQualifiedColField(columnDef));
-          html = html.replace(uiGridConstants.COL_FIELD, 'grid.getCellValue(row, col)');
-
-          $scope.grid = Grid;
-
-          $scope.row = row;
-
-          var compiledElementFn = $compile(html);
-
-          var datos;
-          // compiledElementFn(row.grid.appScope, function(clonedElement, scope) {
-          //   console.log(clonedElement);
-          //   datos = clonedElement;
-          // });
-
-          // console.log(datos);
-          //
-          // var par = $parse('row');
-          // var afpar = par(row.grid.appScope);
-
-          //console.log('Caso $parse:' + par);
-          // console.log('Caso $parse:' + afpar);
-          //console.log('Caso $eval:' + $scope.$eval('grid'));
-
-
-
-
-          return datos;
-        }
-
-        var field =  columnDef.filterField || columnDef.field;
-
-        if(_isFunction(field)) {
-          return field(entity);
-        }
-
-        if(field.indexOf('.') < 0 ) return entity[field];
-
-        var property = entity;
-        var arrayOfLevels = field.split('.');
-
-        for( var i=0; i < arrayOfLevels.length ; i++ ) {
-          if(!property) return '';
-          property = property[arrayOfLevels[i]];
-        }
-
-        return property;
-
-        function _isFunction(functionToCheck) {
-          var getType = {};
-          return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
-        }
-      }
 
       function singleFilter( renderableRows ) {
         if (!uiGridFilterValueService.filterValue) {
           return renderableRows;
         }
-        var matcher = createFilterRegex(uiGridFilterValueService.filterValue, true);
+
+        var matcher = _createFilterRegex(uiGridFilterValueService.filterValue, true);
+
         renderableRows.forEach( function( row ) {
-          var filterData = concatEntityFPropertiesSelectedAsField(row);
+          var filterData = _concatCellValues(row);
 
           var match = filterData.match(matcher);
-
           if ( !match ){
             row.visible = false;
           }
         });
+
         return renderableRows;
 
-
-        function concatEntityFPropertiesSelectedAsField(row) {
+        function _concatCellValues(row) {
           var concatedProperties = '';
 
           if (row.grid.columns) {
             row.grid.columns.forEach(function (col, idx) {
-              $scope.grid = row.grid;
-              $scope.row = row;
-              $scope.col = col;
-              var res = col.compiledElementFn($scope);
-              console.log(res);
-              var colValue;
-
-              var interpolate = $interpolate( res.html() );
-              colValue = interpolate($scope);
-              concatedProperties = concatedProperties.concat(colValue);
-            })
+              var cellValue = _getRenderedCellValue(row, col);
+              concatedProperties = concatedProperties.concat(cellValue);
+            });
           }
 
           return concatedProperties;
         }
 
+        function _getRenderedCellValue(row, col) {
+          $scope.grid = row.grid;
+          $scope.row = row;
+          $scope.col = col;
 
-        function createFilterRegex (search, caseInsensitive) {
+          var cellTemplate = col.compiledElementFn($scope);
+          var cellValue = $interpolate(cellTemplate.html())($scope);
+          return cellValue;
+        }
+
+
+        function _createFilterRegex (search, caseInsensitive) {
           search = _fnEscapeRegex( search );
           var a = $.map( search.match( /"[^"]+"|[^ ]+/g ) || [''], function ( word ) {
             if ( word.charAt(0) === '"' ) {
